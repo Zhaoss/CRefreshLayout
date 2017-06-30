18973455fff0dc6f23e380d0e33fb0531e56941d
zhaoshuang <zss_@outlook.com>
2017-06-30 19:03:51 +0800

    v2.0
    v2.0

--- app/src/main/java/com/zhaoshuang/crefreshlayout/MainActivity.java
+++ app/src/main/java/com/zhaoshuang/crefreshlayout/MainActivity.java
@@ -6,12 +6,17 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private CRefreshLayout cRefreshLayout;
+    private TextView tv_load;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +27,46 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         int[] datas = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         recyclerView.setAdapter(new MyAdapter(this, datas));
 
         cRefreshLayout.setOnRefreshListener(new CRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
+                Toast.makeText(getApplicationContext(), "触发刷新", Toast.LENGTH_SHORT).show();
                 myHandler.sendEmptyMessageDelayed(0, 2000);
+            }
+        });
+
+        View view = View.inflate(this, R.layout.foot_layout, null);
+        tv_load = (TextView) view.findViewById(R.id.tv_load);
+        int height = (int) getResources().getDimension(R.dimen.dp100);
+        view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, height));
+        cRefreshLayout.bindLoadView(view);
+        cRefreshLayout.setOnCustomLoadListener(new CRefreshLayout.OnCustomLoadListener() {
+            @Override
+            public void onStart(View loadView) {
+                tv_load.setText("下拉加载");
+            }
+
+            @Override
+            public void onMove(View loadView, int dy, int current, int max) {
+                if(current >= loadView.getHeight()){
+                    tv_load.setText("松手即可加载");
+                }else{
+                    tv_load.setText("下拉加载");
+                }
+            }
+
+            @Override
+            public void onUp(View loadView, int currentY) {
+                Log.i("Log.i", "onUp: "+currentY);
+            }
+
+            @Override
+            public void onLoad(View loadView) {
+                tv_load.setText("正在加载中...");
+                myHandler.sendEmptyMessageDelayed(1, 2000);
             }
         });
     }
@@ -37,8 +74,13 @@ public class MainActivity extends AppCompatActivity {
     Handler myHandler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
-            Toast.makeText(getApplicationContext(), "刷新成功", Toast.LENGTH_SHORT).show();
-            cRefreshLayout.setRefreshing(false);
+            if(msg.what == 0){
+                Toast.makeText(getApplicationContext(), "刷新成功", Toast.LENGTH_SHORT).show();
+                cRefreshLayout.setRefreshing(false);
+            }else{
+                Toast.makeText(getApplicationContext(), "加载成功", Toast.LENGTH_SHORT).show();
+                cRefreshLayout.setLoading(false);
+            }
         }
     };
 }
--- crefreshlayout/src/main/java/com/zhaoshuang/crefreshlayout/CRefreshLayout.java
+++ crefreshlayout/src/main/java/com/zhaoshuang/crefreshlayout/CRefreshLayout.java
@@ -9,7 +9,6 @@ import android.support.v4.view.NestedScrollingChild;
 import android.support.v4.view.NestedScrollingChildHelper;
 import android.support.v4.view.NestedScrollingParent;
 import android.support.v4.view.NestedScrollingParentHelper;
-import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,11 +28,17 @@ public class CRefreshLayout extends FrameLayout implements NestedScrollingParent
     private OnRefreshListener onRefreshListener;
     private ValueAnimator valueAnimator;
     private OnCustomRefreshListener onCustomRefreshListener;
-    private int maxScrollY = -1;
+    private OnCustomLoadListener onCustomLoadListener;
+    private int maxRefreshScrollY = -1;
+    private int maxLoadScrollY = -1;
     private float scrollRatio = 0.4f;
     private int refreshY;
+    private int loadY;
     private boolean mRefreshState = true;
     private NestedScrollingChildHelper mNestedScrollingChildHelper;
+    private View mLoadLayout;
+    private boolean mLoading;
+    private boolean isFistLayout = true;
 
     public CRefreshLayout(Context context) {
         super(context);
@@ -61,7 +66,11 @@ public class CRefreshLayout extends FrameLayout implements NestedScrollingParent
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
 
-        setRefreshLayout(false);
+        if(isFistLayout) {
+            isFistLayout = false;
+            setRefreshLayout(false);
+            setLoadLayout(false);
+        }
     }
 
     @Override
@@ -72,14 +81,7 @@ public class CRefreshLayout extends FrameLayout implements NestedScrollingParent
         bindRefreshView(new RefreshView(getContext()));
     }
 
-    /**
-     * 判断控件是否可以滑动
-     */
-    private boolean canChildScrollUp(View view) {
-        return ViewCompat.canScrollVertically(view, -1);
-    }
-
-    private void openAnim(){
+    private void refreshOpenAnim(){
 
         if(valueAnimator == null) {
             valueAnimator = ValueAnimator.ofFloat(mRefreshView.getY(), 0);
@@ -102,7 +104,53 @@ public class CRefreshLayout extends FrameLayout implements NestedScrollingParent
         }
     }
 
-    private void closeAnim(){
+    private void loadOpenAnim(){
+
+        if(valueAnimator == null) {
+            valueAnimator = ValueAnimator.ofFloat(mLoadLayout.getY(), mTarget.getHeight()-mLoadLayout.getHeight());
+            valueAnimator.setDuration(100);
+            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    float value = (float) animation.getAnimatedValue();
+                    mLoadLayout.setY(value);
+                    mTarget.setY(value-mTarget.getHeight());
+                }
+            });
+            valueAnimator.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    valueAnimator = null;
+                }
+            });
+            valueAnimator.start();
+        }
+    }
+
+    private void loadCloseAnim(){
+
+        if(valueAnimator == null) {
+            valueAnimator = ValueAnimator.ofFloat(mLoadLayout.getY(), mTarget.getHeight());
+            valueAnimator.setDuration(100);
+            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    float value = (float) animation.getAnimatedValue();
+                    mLoadLayout.setY(value);
+                    mTarget.setY(value-mTarget.getHeight());
+                }
+            });
+            valueAnimator.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    valueAnimator = null;
+                }
+            });
+            valueAnimator.start();
+        }
+    }
+
+    private void refreshCloseAnim(){
 
         if(valueAnimator == null) {
             valueAnimator = ValueAnimator.ofFloat(mRefreshView.getY(), -mRefreshView.getHeight());
@@ -126,11 +174,21 @@ public class CRefreshLayout extends FrameLayout implements NestedScrollingParent
     }
 
     private void onRefresh(){
-        if(onRefreshListener != null) onRefreshListener.onRefresh();
-        if(onCustomRefreshListener != null) onCustomRefreshListener.onRefresh(mRefreshView);
+        if(onRefreshListener != null) {
+            onRefreshListener.onRefresh();
+        }
+        if(onCustomRefreshListener != null) {
+            onCustomRefreshListener.onRefresh(mRefreshView);
+        }
         if(mRefreshView instanceof CRefreshView){
             CRefreshView anim = (CRefreshView) mRefreshView;
             anim.startAnim();
+        }
+    }
+
+    private void onLoad(){
+        if(onCustomLoadListener != null) {
+            onCustomLoadListener.onLoad(mLoadLayout);
         }
     }
 
@@ -143,6 +201,20 @@ public class CRefreshLayout extends FrameLayout implements NestedScrollingParent
                 mTarget.setY(layoutParams.height);
             } else {
                 mRefreshView.setY(-layoutParams.height);
+                mTarget.setY(0);
+            }
+        }
+    }
+
+    private void setLoadLayout(boolean isOpen){
+
+        if(mTarget!=null && mLoadLayout!=null) {
+            ViewGroup.LayoutParams layoutParams = mLoadLayout.getLayoutParams();
+            if (isOpen) {
+                mLoadLayout.setY(mTarget.getHeight()-layoutParams.height);
+                mTarget.setY(-layoutParams.height);
+            } else {
+                mLoadLayout.setY(mTarget.getHeight());
                 mTarget.setY(0);
             }
         }
@@ -162,29 +234,58 @@ public class CRefreshLayout extends FrameLayout implements NestedScrollingParent
         addView(refreshView);
 
         refreshY = mRefreshView.getLayoutParams().height;
-        setRefreshLayout(false);
+    }
+
+    /**
+     * 绑定loadView
+     */
+    public void bindLoadView(View loadView) {
+
+        if(mLoadLayout != null){
+            removeView(mLoadLayout);
+        }
+        this.mLoadLayout = loadView;
+        addView(mLoadLayout);
+
+        loadY = -mLoadLayout.getLayoutParams().height;
+    }
+
+    /**
+     * 设置加载状态
+     */
+    public void setLoading(boolean loading){
+        if(loading){
+            loadOpenAnim();
+        }else{
+            loadCloseAnim();
+        }
+        this.mLoading = loading;
     }
 
     /**
      * 设置刷新状态
      */
-    public void setRefreshing(boolean mRefreshing){
-        if(mRefreshing){
-            openAnim();
+    public void setRefreshing(boolean refreshing){
+        if(refreshing){
+            refreshOpenAnim();
+            if(mRefreshView instanceof CRefreshView){
+                CRefreshView cRefreshView = (CRefreshView) mRefreshView;
+                cRefreshView.startAnim();
+            }
         }else{
             if(mRefreshView instanceof CRefreshView){
                 CRefreshView cRefreshView = (CRefreshView) mRefreshView;
                 cRefreshView.stopAnim(new CRefreshView.OnAnimationListener(){
                     @Override
                     public void onAnimationEnd(Animation animation) {
-                        closeAnim();
+                        refreshCloseAnim();
                     }
                 });
             }else {
-                closeAnim();
+                refreshCloseAnim();
             }
         }
-        this.mRefreshing = mRefreshing;
+        this.mRefreshing = refreshing;
     }
 
     /**
@@ -201,6 +302,39 @@ public class CRefreshLayout extends FrameLayout implements NestedScrollingParent
          * 触发刷新
          */
         void onRefresh();
+    }
+
+    /**
+     * 设置上拉加载控件的生命周期监听
+     */
+    public void setOnCustomLoadListener(OnCustomLoadListener listener) {
+        this.onCustomLoadListener = listener;
+    }
+
+    public interface OnCustomLoadListener{
+        /**
+         * 开始上拉刷新
+         */
+        void onStart(View loadView);
+
+        /**
+         * 上拉拖拽中
+         * @param dy 本次移动距离
+         * @param current 已经拖拽距离
+         * @param max 总可拖拽距离
+         */
+        void onMove(View loadView, int dy, int current, int max);
+
+        /**
+         * 手势抬起
+         * @param currentY 已经拖拽距离
+         */
+        void onUp(View loadView, int currentY);
+
+        /**
+         * 触发加载更多
+         */
+        void onLoad(View loadView);
     }
 
     /**
@@ -240,32 +374,55 @@ public class CRefreshLayout extends FrameLayout implements NestedScrollingParent
     /**
      * 判断是否刷新中
      */
-    public boolean ismRefreshing(){
+    public boolean isRefreshing(){
         return mRefreshing;
     }
 
     /**
-     * 设置最大拖拽距离
+     * 判断是否加载中
+     */
+    public boolean isLoading(){
+        return mLoading;
+    }
+
+    /**
+     * 限制下拉刷新时最大拖拽距离
      * @param max 值<0 表示不限制拖拽距离
      */
-    public void setMaxScroll(int max){
-        this.maxScrollY = max;
+    public void setRefreshMaxScroll(int max){
+        this.maxRefreshScrollY = max;
+    }
+
+    /**
+     * 限制上拉加载时最大拖拽距离
+     * @param max 值<0 表示不限制拖拽距离
+     */
+    public void setLoadMaxScroll(int max){
+        this.maxLoadScrollY = max;
     }
 
     /**
      * 设置拖拽速度比, 即下拉拖拽时, 距离移动距离比率
-     * @param scrollRatio 数值小于1, 下拉慢, 数值大于1, 下拉速度快
+     * @param scrollRatio 数值小于1, 下拉速度慢, 数值大于1, 下拉速度快
      */
-    public void setMaxScroll(float scrollRatio){
+    public void setScrollRatio(float scrollRatio){
         this.scrollRatio = scrollRatio;
     }
 
     /**
-     * 设置刷新触发点, 拖拽距离超过此值, 即满足刷新条件
+     * 设置下拉刷新触发点, 拖拽距离超过此值, 即满足刷新条件
      * @param refreshY 默认值为刷新控件的高度
      */
     public void setRefreshY(int refreshY){
         this.refreshY = refreshY;
+    }
+
+    /**
+     * 设置上拉加载触发点, 上拉距离超过此值, 即满足加载条件
+     * @param loadY 默认值为加载控件的高度
+     */
+    public void setLoadY(int loadY){
+        this.loadY = loadY;
     }
 
     /**
@@ -278,24 +435,67 @@ public class CRefreshLayout extends FrameLayout implements NestedScrollingParent
 
     //------------------------------------ NestedScrollingParent ---------------------
 
+    boolean isFirstMove = true;
     @Override
     public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
 
-        if(mRefreshView !=null) {
-            float slideY = -dy*scrollRatio;
-            if (slideY>0 && !canChildScrollUp(mTarget)) {
-                scroll(slideY);
-            }
-            if(slideY<0 && mRefreshView.getY()>-mRefreshView.getHeight()){
-                scroll(slideY);
-                consumed[1]=dy;//吃掉移动距离, 不给子view
+        if(!mRefreshing && !mLoading) {
+            if (mRefreshView != null) {
+                float slideY = -dy * scrollRatio;
+                if (slideY > 0 && !mTarget.canScrollVertically(-1)) {
+                    scrollRefresh(slideY);
+                    if (isFirstMove) {
+                        isFirstMove = false;
+                        if (onCustomRefreshListener != null)
+                            onCustomRefreshListener.onStart(mRefreshView);
+                    }
+                } else if (slideY < 0 && mRefreshView.getY() > -mRefreshView.getHeight()) {
+                    scrollRefresh(slideY);
+                    consumed[1] = dy;//吃掉移动距离, 不给子view
+                }
             }
 
-            if(onCustomRefreshListener != null) onCustomRefreshListener.onMove(mRefreshView, dy, (int)mTarget.getY(), -1);
+            if (mLoadLayout != null) {
+                float slideY = -dy * scrollRatio;
+                if (slideY < 0 && !mTarget.canScrollVertically(1)) {
+                    scrollLoad(slideY);
+                    if (isFirstMove) {
+                        isFirstMove = false;
+                        if (onCustomLoadListener != null) onCustomLoadListener.onStart(mLoadLayout);
+                    }
+                } else if (slideY > 0 && mLoadLayout.getY() < mTarget.getHeight()) {
+                    scrollLoad(slideY);
+                    consumed[1] = dy;//吃掉移动距离, 不给子view
+                }
+            }
         }
     }
 
-    private void scroll(float slideY){
+    private void scrollLoad(float slideY){
+
+        float y1 = mLoadLayout.getY() + slideY;
+        float y2 = mTarget.getY() + slideY;
+
+        if(maxLoadScrollY > 0) {
+            if (y1 < mTarget.getHeight() - mLoadLayout.getHeight()) {
+                y1 = mTarget.getHeight() - mLoadLayout.getHeight();
+            }
+            if (y2 < -mLoadLayout.getHeight()) {
+                y2 = -mLoadLayout.getHeight();
+            }
+        }
+
+
+
+        mLoadLayout.setY(y1);
+        mTarget.setY(y2);
+
+        if(onCustomLoadListener!=null && !isFirstMove) {
+            onCustomLoadListener.onMove(mLoadLayout, (int) slideY, (int)-mTarget.getY(), maxLoadScrollY);
+        }
+    }
+
+    private void scrollRefresh(float slideY){
 
         float y1 = mRefreshView.getY() + slideY;
         float y2 = mTarget.getY() + slideY;
@@ -303,32 +503,57 @@ public class CRefreshLayout extends FrameLayout implements NestedScrollingParent
         if (y1 < -mRefreshView.getHeight()) y1 = -mRefreshView.getHeight();
         if (y2 < 0) y2 = 0;
 
-        if(maxScrollY > 0) {
-            if (y1 > maxScrollY - mRefreshView.getHeight()) y1 = maxScrollY - mRefreshView.getHeight();
-            if (y2 > maxScrollY) y2 = maxScrollY;
+        if(maxRefreshScrollY > 0) {
+            if (y1 > maxRefreshScrollY - mRefreshView.getHeight()) {
+                y1 = maxRefreshScrollY - mRefreshView.getHeight();
+            }
+            if (y2 > maxRefreshScrollY) {
+                y2 = maxRefreshScrollY;
+            }
         }
 
         mRefreshView.setY(y1);
         mTarget.setY(y2);
+
+        if(onCustomRefreshListener!=null && !isFirstMove) {
+            onCustomRefreshListener.onMove(mRefreshView, (int) slideY, (int)mTarget.getY(), maxRefreshScrollY);
+        }
     }
 
     @Override
     public void onStopNestedScroll(View target){
 
         mNestedScrollingParentHelper.onStopNestedScroll(target);
+        isFirstMove = true;
 
-        if(mTarget.getY() > 0) {
-            if (!mRefreshing) {
+        if(!mRefreshing && !mLoading) {
+            if (mTarget.getY() > 0) {
                 if (mTarget.getY() >= refreshY) {
                     mRefreshing = true;
                     setRefreshLayout(true);
                     onRefresh();
                 } else {
-                    closeAnim();
                     mRefreshing = false;
+                    refreshCloseAnim();
+                }
+                if (onCustomRefreshListener != null && mTarget.getY() != 0) {
+                    onCustomRefreshListener.onUp(mRefreshView, (int) mTarget.getY());
                 }
             }
-            if(onCustomRefreshListener != null) onCustomRefreshListener.onUp(mRefreshView, (int)mTarget.getY());
+
+            if (mTarget.getY() < 0) {
+                if (mTarget.getY() <= loadY) {
+                    mLoading = true;
+                    setLoadLayout(true);
+                    onLoad();
+                } else {
+                    mLoading = false;
+                    loadCloseAnim();
+                }
+                if (onCustomLoadListener != null && mTarget.getY() != 0) {
+                    onCustomLoadListener.onUp(mLoadLayout, (int) -mTarget.getY());
+                }
+            }
         }
     }
 
@@ -353,7 +578,6 @@ public class CRefreshLayout extends FrameLayout implements NestedScrollingParent
 
     @Override
     public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes) {
-        if(onCustomRefreshListener != null) onCustomRefreshListener.onStart(mRefreshView);
         return mRefreshState;
     }
 
